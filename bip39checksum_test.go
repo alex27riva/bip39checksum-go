e077d076329cc08b92104802b206fd0f396e45be
Alessandro Riva <alex27riva@gmail.com>
2023-02-20 11:10:07 +0100

    Fix test

--- bip39checksum_test.go
+++ bip39checksum_test.go
@@ -1,19 +1,17 @@
 package main
 
 import (
-	"testing"
 	"reflect"
+	"testing"
 )
-
 
 func TestMain(t *testing.T) {
 	tests := []struct {
 		input string
 		want  []string
 	}{
-		{"crowd flip sign buyer truly sail balcony amazing spy garbage cool during decide female phone lawn faith rely barrel celery urban bridge width", []string{"word1", "word2", "word3", "word4", "word5", "word6", "word7", "word8", "word9", "word10", "word11"}},
-		{"twenty three words mnemonic", []string{"word1", "word2", "word3", "word4", "word5", "word6", "word7", "word8", "word9", "word10", "word11", "word12", "word13", "word14", "word15", "word16", "word17", "word18", "word19", "word20", "word21", "word22", "word23"}},
-		{"invalid mnemonic", []string{}},
+		{"crowd flip sign buyer truly sail balcony amazing spy garbage cool", []string{"ability", "advance", "air", "alien", "anger", "april", "aspect", "attack", "bachelor", "because", "best", "blind", "bone", "brick", "broccoli", "buyer", "canvas", "captain", "cereal", "champion", "claim", "click", "coast", "conduct", "coral", "crop", "cross", "curtain", "decrease", "demise", "detect", "dinosaur", "donkey", "dry", "dust", "electric", "enable", "enhance", "ethics", "excess", "exotic", "fatigue", "fever", "fish", "flight", "forum", "foster", "garlic", "gasp", "glad", "govern", "gun", "harvest", "hello", "horror", "humor", "icon", "inhale", "insect", "involve", "joke", "know", "ladder", "leaf", "liberty", "lobster", "lounge", "maid", "mean", "merry", "mistake", "more", "motor", "nation", "nominee", "north", "often", "olympic", "organ", "panda", "peanut", "permit", "pig", "plunge", "poverty", "prevent", "protect", "purity", "ramp", "ready", "reflect", "resemble", "rigid", "roof", "rule", "say", "scissors", "second", "setup", "shove", "silent", "slogan", "slush", "solid", "speak", "squeeze", "sting", "strike", "submit", "swim", "tag", "task", "ticket", "today", "top", "tree", "trick", "turn", "uncover", "urban", "valley", "video", "vocal", "wage", "wear", "wink", "wolf", "zone"}},
+		{"grit swim bronze output various fiscal cement normal warrior wild area hire refuse rely group culture phone cliff unfair curve claw secret reward", []string{"arch", "devote", "fix", "jewel", "lucky", "result", "sea", "wage"}},
 	}
 
 	for _, test := range tests {
