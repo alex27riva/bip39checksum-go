fcb56fa749df4be96e68f0e73cca94862852f232
Alessandro Riva <alex27riva@gmail.com>
2025-01-05 13:50:27 +0100

    Better code - Trim spaces - Add color

--- bip39checksum.go
+++ bip39checksum.go
@@ -3,39 +3,55 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"github.com/fatih/color"
 	"github.com/tyler-smith/go-bip39"
 	"os"
+	"strings"
+)
+
+const (
+	prompt = "Please enter 11 or 23 BIP39 words: "
 )
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Please enter 11 or 23 BIP39 words: ")
-	input, _ := reader.ReadString('\n')
-	words := brute(input)
-	mnemonicLen := len(words)
+	fmt.Print(color.BlueString(prompt))
 
-	if mnemonicLen > 0 {
-		fmt.Printf("\nThere are %v last words valid for this mnemonic: \n\n", mnemonicLen)
-		printWords(words)
-	} else {
-		fmt.Println("The mnemonic phrase you entered is not valid.")
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		color.Red("Error reading input:", err)
+		return
+	}
+
+	mnemonic := strings.TrimSpace(input)
+	nWords := countWords(mnemonic)
+
+	switch nWords {
+	case 11, 23:
+		possibleWords := brute(mnemonic)
+		color.Blue("\nThere are %v last words valid for this mnemonic: \n", len(possibleWords))
+		printWords(possibleWords)
+	case 12, 24:
+		color.Green("You already have the last word!")
+	default:
+		color.Red("The mnemonic phrase you entered is not valid.")
 	}
 
 }
 
 func brute(mnemonic string) []string {
 	english := bip39.GetWordList()
-	var valid_words []string
+	var validWords []string
 
 	for _, word := range english {
 		current := mnemonic + " " + word
 		isValid := bip39.IsMnemonicValid(current)
 
 		if isValid {
-			valid_words = append(valid_words, word)
+			validWords = append(validWords, word)
 		}
 	}
-	return valid_words
+	return validWords
 }
 
 func printWords(arr []string) {
@@ -44,4 +60,9 @@ func printWords(arr []string) {
 	}
 	fmt.Println()
 
+}
+
+func countWords(s string) int {
+	words := strings.Fields(s)
+	return len(words)
 }
--- go.mod
+++ go.mod
@@ -1,7 +1,15 @@
 module bip39checksum-go
 
-go 1.19
+go 1.23.4
 
-require github.com/tyler-smith/go-bip39 v1.1.0
+require (
+	github.com/fatih/color v1.18.0
+	github.com/tyler-smith/go-bip39 v1.1.0
+)
 
-require golang.org/x/crypto v0.31.0 // indirect
+require (
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	golang.org/x/crypto v0.31.0 // indirect
+	golang.org/x/sys v0.28.0 // indirect
+)
